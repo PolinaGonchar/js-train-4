3d46fee3a30ae3b14a248faf253a6ded2dc3e4ec
PolinaGonchar <gonchar.polina06@gmail.com>
2024-08-08 21:25:16 +0200

    Wrote solutions to tasks

--- index.js
+++ index.js
@@ -1,124 +1,125 @@
-// Задача 1: Напишіть функцію, яка перетворює рядок в число, використовуючи Number()
+// Task 1: Write a function that converts a string to a number using Number()
 
 function stringToNumber(str) {
-  // Використовуємо Number() для перетворення рядка в число
-  // Повертаємо отримане число
+  // Use Number() to convert the string to a number
+  return Number(str);
 }
 
-console.log("Завдання 1 ====================================");
-console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
+console.log("Task 1 ====================================");
+console.log('stringToNumber("42")', stringToNumber("42")); // Outputs 42
 
-// Задача 2: Напишіть функцію, яка визначає, чи є два числа майже однаковими, використовуючи Number.EPSILON
+// Task 2: Write a function that determines if two numbers are almost the same using Number.EPSILON
 
 function isAlmostSame(num1, num2) {
-  // Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
+  let difference = Math.abs(num1 - num2);
+  return difference <= Number.EPSILON;
 }
 
-console.log("Завдання 2 ====================================");
-console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // Виведе true
+console.log("Task 2 ====================================");
+console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // Outputs true
 
-// Задача 3: Напишіть функцію, яка перевіряє, чи є число безпечним цілим числом
+// Task 3: Write a function that checks if a number is a safe integer
 
 function isSafeInteger(num) {
-  // Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
+  return Number.isInteger(num) && num <= Number.MAX_SAFE_INTEGER && num >= Number.MIN_SAFE_INTEGER;
 }
 
-console.log("Завдання 3 ====================================");
+console.log("Task 3 ====================================");
 console.log(
   "isSafeInteger(Number.MAX_SAFE_INTEGER + 1)",
   isSafeInteger(Number.MAX_SAFE_INTEGER + 1)
-); // Виведе false
+); // Outputs false
 
-// Задача 4: Використовуючи результати Задачі 3, напишіть функцію, яка перевіряє, чи є число небезпечним цілим числом
+// Task 4: Using the results from Task 3, write a function that checks if a number is an unsafe integer
 
 function isUnsafeInteger(num) {
-  // Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
+  return !isSafeInteger(num);
 }
 
-console.log("Завдання 4 ====================================");
+console.log("Task 4 ====================================");
 console.log(
   "isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)",
   isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)
-); // Виведе true
+); // Outputs true
 
-// Задача 5: Напишіть функцію, яка перевіряє, чи є число надто великим для представлення в JavaScript
+// Task 5: Write a function that checks if a number is too large to be represented in JavaScript
 
 function isTooLarge(num) {
-  // Перевіряємо, чи є число більшим за MAX_VALUE
+  return num > Number.MAX_VALUE;
 }
 
-console.log("Завдання 5 ====================================");
+console.log("Task 5 ====================================");
 console.log(
   "isTooLarge(Number.MAX_VALUE * 2)",
   isTooLarge(Number.MAX_VALUE * 2)
-); // Виведе true
+); // Outputs true
 
-// Задача 6: Напишіть функцію, яка перевіряє, чи є число меншим чим  найменше можливе числове значення в JavaScript
+// Task 6: Write a function that checks if a number is smaller than the smallest possible numeric value in JavaScript
 
 function isAlmostZero(num) {
-  // Перевіряємо, чи є число більше 0, але все ще менше за Number.MIN_VALUE
+  return num > 0 && num < Number.MIN_VALUE;
 }
 
-console.log("Завдання 6 ====================================");
+console.log("Task 6 ====================================");
 console.log(
   "isAlmostZero(Number.MIN_VALUE / 2)",
   isAlmostZero(Number.MIN_VALUE / 2)
-); // Виведе false
-console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); // Виведе false
+); // Outputs false
+console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); // Outputs false
 
-// Задача 7: Напишіть функцію, яка перевіряє, чи є значення цілим числом
+// Task 7: Write a function that checks if a value is an integer
 
 function checkIsInteger(num) {
-  // Використовуємо вбудовану функцію Number.isInteger(), щоб перевірити, чи є значення цілим числом
+  return Number.isInteger(num);
 }
 
-console.log("Завдання 7 ====================================");
-console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
+console.log("Task 7 ====================================");
+console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Outputs false
 
-// Задача 8: Напишіть функцію, яка перевіряє, чи є значення безпечним цілим числом
+// Task 8: Write a function that checks if a value is a safe integer
 
 function checkIsSafeInteger(num) {
-  // Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
+  return Number.isSafeInteger(num);
 }
 
-console.log("Завдання 8 ====================================");
+console.log("Task 8 ====================================");
 console.log(
   "checkIsSafeInteger(Math.pow(2, 53))",
   checkIsSafeInteger(Math.pow(2, 53))
-); // Виведе false
+); // Outputs false
 
-// Задача 9: Напишіть функцію, яка конвертує число в рядок з експоненційним представленням
+// Task 9: Write a function that converts a number to a string in exponential notation
 
 function convertToExponential(num) {
-  // Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
+  return num.toExponential();
 }
 
-console.log("Завдання 9 ====================================");
-console.log("convertToExponential(42)", convertToExponential(42)); // Виведе "4.2e+1"
+console.log("Task 9 ====================================");
+console.log("convertToExponential(42)", convertToExponential(42)); // Outputs "4.2e+1"
 
-// Задача 10: Напишіть функцію, яка конвертує число в рядок з фіксованою кількістю знаків після коми
+// Task 10: Write a function that converts a number to a string with a fixed number of decimals
 
 function convertToFixed(num, precision) {
-  // Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
+  return num.toFixed(precision);
 }
 
-console.log("Завдання 10 ====================================");
-console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Виведе "42.99"
+console.log("Task 10 ====================================");
+console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Outputs "42.99"
 
-// Задача 11: Напишіть функцію, яка конвертує число в рядок
+// Task 11: Write a function that converts a number to a string
 
 function convertToString(num) {
-  // Використовуємо метод toString(), щоб конвертувати число в рядок
+  return num.toString();
 }
 
-console.log("Завдання 11 ====================================");
-console.log("convertToString(42)", convertToString(42)); // Виведе "42"
+console.log("Task 11 ====================================");
+console.log("convertToString(42)", convertToString(42)); // Outputs "42"
 
-// Задача 12: Напишіть функцію, яка окргугляє число до вказаної довжини
+// Task 12: Write a function that rounds a number to a specified precision
 
 function convertToPrecision(num, precision) {
-  // Використовуємо метод toPrecision(), щоб округлити число до вказаної довжини
+  return num.toPrecision(precision);
 }
 
-console.log("Завдання 12 ====================================");
-console.log("convertToPrecision(42.9876, 2)", convertToPrecision(42.9876, 2)); // Виведе "43"
+console.log("Task 12 ====================================");
+console.log("convertToPrecision(42.9876, 2)", convertToPrecision(42.9876, 2)); // Outputs "43"
